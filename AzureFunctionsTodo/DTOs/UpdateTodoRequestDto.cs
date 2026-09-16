c5d6628faad40c39e8a4824f7650ca5bd82574b1
Jonathan Krall <jonathan.krall@lexicon.se>
2026-09-16 11:49:14 +0200

    Added DTOs for Create and Update

---
+++ AzureFunctionsTodo/DTOs/UpdateTodoRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AzureFunctionsTodo.DTOs
+{
+    public class UpdateTodoRequestDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
