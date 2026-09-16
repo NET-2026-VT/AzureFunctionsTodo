7d0884ab6162464c40ba05e040af56502afabe54
Jonathan Krall <jonathan.krall@lexicon.se>
2026-09-16 11:44:08 +0200

    Created ToDoItem

---
+++ AzureFunctionsTodo/Models/ToDoItem.cs
@@ -0,0 +1,22 @@
+﻿using Azure;
+using Azure.Data.Tables;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AzureFunctionsTodo.Models
+{
+    public class ToDoItem : ITableEntity
+    {
+        public string PartitionKey { get; set; } = "TODO";
+        public string RowKey { get; set; } = Guid.NewGuid().ToString();
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow; 
+    }
+}
